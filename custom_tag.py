1a02b65d3eca8e00e52adfd217490bcea54cd34e
Sean Wu <panda@ms5.url.com.tw>
2018-07-18 15:41:29 +0800

    Create custom_tag.py

---
+++ custom_tag.py
@@ -0,0 +1,6 @@
+  The app should contain a templatetags directory, at the same level as models.py, views.py, etc. 
+  If this doesn’t already exist, create it
+  don’t forget the __init__.py file to ensure the directory is treated as a Python package.
+  
+  in the html
+  {% load sap_tag %}
