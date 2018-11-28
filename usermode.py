30cff26390d9b77c879694ab9ccc0e8be56d7470
Sean Wu <panda@ms5.url.com.tw>
2018-11-28 16:05:04 +0800

    Update usermode.py

--- usermode.py
+++ usermode.py
@@ -7,17 +7,21 @@
 # models.py
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-# 
 class User(AbstractUser):
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     
 # settings.py
-
 AUTH_USER_MODEL = 'usblog.User'  # <<appname>>.User
 
 
 # python manage.py makemigrations usblog
 # python manage.py migrate usblog
+
+
+
+#admin.py
+from usblog.models import User
+admin.site.register(User)
+
