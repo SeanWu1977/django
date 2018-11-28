25a5b0810c1a5d70dd43db17ecc52d26294fe89a
Sean Wu <panda@ms5.url.com.tw>
2018-11-28 14:31:35 +0800

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,19 @@
+# 控制密碼簡檢規
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 6,
+        }
+    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
+]
