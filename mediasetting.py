7a41aee3eee68cbfcc2f82e8fbdfc54f3128822a
Sean Wu <panda@ms5.url.com.tw>
2018-11-30 15:05:25 +0800

    Update mediasetting.py

--- mediasetting.py
+++ mediasetting.py
@@ -19,3 +19,27 @@ MEDIA_URL = '/media/'
 
 #templates
 <a href="/media/xls/a.xls">下載模板</a>
+
+
+# 檔案名稱加解密
+from django.core import signing
+# 加密
+value = signing.dumps(filename)
+# 解密
+org = signing.loads(value)
+
+
+# 可以不限制在media下
+from django.http import FileResponse  
+def file_down(request):  
+    from django.http import FileResponse
+    from django.core import signing
+    value = 'ImEueGxzIg:1gScoL:x4-KqHaxIUq7-zD6m0241g_pP8g'
+    filename = signing.loads(value)
+    dir = '<absolute path>'    
+    full_name = '{}/{}'.format(dir,filename)
+    file=open(full_name,'rb')
+    response =FileResponse(file)  
+    response['Content-Type']='application/octet-stream'  
+    response['Content-Disposition']='attachment;filename="example.tar.gz"'  
+    return response 
